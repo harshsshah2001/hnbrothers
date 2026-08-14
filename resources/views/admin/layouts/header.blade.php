cb7a8225713cac4951ebe5daf2aa488b654bac47
Harsh <harshsshah2001@gmail.com>
2026-08-14 08:51:11 +0530

    harsh

--- resources/views/admin/layouts/header.blade.php
+++ resources/views/admin/layouts/header.blade.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8" />
     {{-- <title>Fair D</title> --}}
     <title>
-        title
+        Admin Dashboard 
     </title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
